13e4a9f9c58ad82fa9511d36e067d99e66b5ad79
maechabin <mail@chab.in>
2015-08-01 22:17:15 +0900

    add license

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,32 @@ var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
+var header = require('gulp-header');
 
-var src = './src/konami.js';
+var src = "./src/*.js";
+var pkg = require("./package.json");
+var banner = ["/**",
+  " * <%= pkg.name %> - <%= pkg.description %>",
+  " * @version v<%= pkg.version %>",
+  " * @author <%= pkg.author %>",
+  " * @license <%= pkg.license %>",
+	" * @repository <%= pkg.repository.url %>",
+  " */",
+  ""].join("\n");
 
-gulp.task('babel', function () {
+gulp.task("babel", function () {
 	gulp.src(src)
 		.pipe(babel())
-		.pipe(concat('konami.min.js'))
-		.pipe(uglify({preserveComments: 'some'}))
-		.pipe(gulp.dest('./dist', {cwd: './'}));
+		.pipe(header(banner, {pkg: pkg}))
+		.pipe(gulp.dest("./dist"), {cwd: "./"})
+		.pipe(concat("konami.min.js"))
+		.pipe(uglify({preserveComments: "some"}))
+		.pipe(header(banner, {pkg: pkg}))
+		.pipe(gulp.dest("./dist", {cwd: "./"}));
 });
 
-gulp.task('watch', function() {
-	gulp.watch(src, ['default']);
+gulp.task("watch", function() {
+	gulp.watch(src, ["default"]);
 });
 
-gulp.task('default', ['babel']);
+gulp.task("default", ["babel"]);
